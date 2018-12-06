85d690b0fafc04e6072f2c7e1117ebb24a1abb9e
Devedse <davydavidse@gmail.com>
2018-12-06 18:26:41 +0100

    Added grid

---
+++ AivdKerstPuzzel2018/Helpers/Gridje.cs
@@ -0,0 +1,23 @@
+﻿namespace AivdKerstPuzzel2018.Helpers
+{
+    public class Gridje<T>
+    {
+        private readonly T[,] innerArray;
+
+        public int Width { get; }
+        public int Height { get; }
+
+        public Gridje(int width, int height)
+        {
+            innerArray = new T[width, height];
+            Width = width;
+            Height = height;
+        }
+
+        public T this[int x, int y]
+        {
+            get => innerArray[x, y];
+            set => innerArray[x, y] = value;
+        }
+    }
+}
--- AivdKerstPuzzel2018/PrePuzzle/PrePuzzleSolver.cs
+++ AivdKerstPuzzel2018/PrePuzzle/PrePuzzleSolver.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AivdKerstPuzzel2018.Helpers;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,14 +7,39 @@ namespace AivdKerstPuzzel2018.PrePuzzle
 {
     public class PrePuzzleSolver
     {
+        private List<int> sumList;
+        private List<int> productList;
+
         public PrePuzzleSolver()
         {
-            
+            sumList = new List<int>()
+            {
+                22,
+                67,
+                59,
+                56,
+                77,
+                72,
+                30,
+                67
+            };
+
+            productList = new List<int>()
+            {
+                216,
+                67200,
+                24840,
+                21450,
+                122892,
+                95760,
+                3150,
+                61560
+            };
         }
 
         public void Go()
         {
-
+            var grid = new Gridje<int>(5, 8);
         }
     }
 }
