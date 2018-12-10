462a1bb945fcc4d85a1048905b18d86651cb33bf
Devedse <davydavidse@gmail.com>
2018-12-10 09:17:29 +0100

    Added char value helper

---
+++ AivdKerstPuzzel2018/Helpers/CharValueHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AivdKerstPuzzel2018.Helpers
+{
+    public static class CharValueHelper
+    {
+        private static char[] CharArray = "abcdefghijklmnopqrstuvwxyz".ToCharArray();
+
+        private static Dictionary<char, int> IntArray = CharArray.Select((t, i) => new { I = i, Char = t }).ToDictionary(t => t.Char, t => t.I);
+
+        public static char IntToChar(int i)
+        {
+            while (i < 0)
+            {
+                i += 26;
+            }
+            return (CharArray[i % 26]);
+        }
+
+        public static int CharToInt(char c)
+        {
+            if (IntArray.TryGetValue(c, out int val))
+            {
+                return val;
+            }
+            throw new InvalidOperationException($"Char {c} is not supported by the CharToInt method in the {nameof(CharValueHelper)}");
+        }
+    }
+}
